db6fab4e1a8ca4f4b293d30126b90dd5201b84fc
MarcoV <marcovirinni@gmail.com>
2026-07-27 13:48:36 -0300

    add fecha nacimiento formato

--- Beneficiario/admin.py
+++ Beneficiario/admin.py
@@ -3,7 +3,19 @@ from django.core.exceptions import ValidationError
 from django import forms
 from .models import Beneficiario
 
+
 class BeneficiarioForm(forms.ModelForm):
+    fecha_nacimiento = forms.DateField(
+        required=False,
+        input_formats=['%d/%m/%Y'],
+        widget=forms.DateInput(
+            format='%d/%m/%Y',
+            attrs={
+                'placeholder': 'DD/MM/AAAA'
+            }
+        )
+    )
+
     class Meta:
         model = Beneficiario
         fields = '__all__'
@@ -15,18 +27,38 @@ class BeneficiarioForm(forms.ModelForm):
 
         if tiene_beneficio and not cual_beneficio:
             raise ValidationError("Debés especificar cuál beneficio recibe.")
+
         if not tiene_beneficio and cual_beneficio:
             raise ValidationError("Marcá 'Tiene beneficio' si especificás cuál.")
 
         return cleaned_data
 
+
 @admin.register(Beneficiario)
 class BeneficiarioAdmin(admin.ModelAdmin):
     form = BeneficiarioForm
-    list_display = ('apellido', 'nombre', 'fecha_nacimiento', 'cuil_dni', 'telefono', 'tipo_vivienda', 'tiene_beneficio')
-    search_fields = ('apellido', 'nombre', 'cuil_dni', 'telefono')
-    list_filter = ('tipo_vivienda', 'tiene_beneficio')
 
-    # --- Esto carga el JS para habilitar/deshabilitar "cual_beneficio" ---
+    list_display = (
+        'apellido',
+        'nombre',
+        'fecha_nacimiento',
+        'cuil_dni',
+        'telefono',
+        'tipo_vivienda',
+        'tiene_beneficio'
+    )
+
+    search_fields = (
+        'apellido',
+        'nombre',
+        'cuil_dni',
+        'telefono'
+    )
+
+    list_filter = (
+        'tipo_vivienda',
+        'tiene_beneficio'
+    )
+
     class Media:
         js = ('admin/js/beneficiario.js',)
